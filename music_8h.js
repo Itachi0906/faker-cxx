898afe60cf36f3b2080068d4e50b2cdf70930da0
Itachi0906 <Itachi0906@users.noreply.github.com>
2025-01-01 14:53:48 +0000

    deploy: 3b6eb423380a389e0f2f0cf7853592c951c93fcf

---
+++ music_8h.js
@@ -0,0 +1,6 @@
+var music_8h =
+[
+    [ "artist", "music_8h.html#a78e9eb50bbeae98d1596a03dacb1466d", null ],
+    [ "genre", "music_8h.html#a75a0b2bf3547ba8e1cae58f1584b4536", null ],
+    [ "songName", "music_8h.html#a4aab326e1500b74cf3e1564345c8a027", null ]
+];
